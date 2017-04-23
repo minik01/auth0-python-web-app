d5ea86ffbcdbc58e37513ce0595a2d5a960f57ed
Minik01 <dominik.wojnar@gmail.com>
2017-04-23 15:52:41 +0200

    Start

--- 00-Starter-Seed/server.py
+++ 00-Starter-Seed/server.py
@@ -81,4 +81,4 @@ def callback_handling():
     return redirect('/dashboard')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000))
+    app.run(host='127.0.0.1', port=os.environ.get('PORT', 3000))
